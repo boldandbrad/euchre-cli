0d67cb61dd37fcb2d29404d5f67de0baf8209d69
Bradley Wojcik <bradleywojcik@gmail.com>
2020-07-01 22:20:03 -0400

    updated setup.py to include author_email, url, python_requires and pypi classifiers

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-# parse version number from euchre-cli/__init__.py:
+# parse version number from euchrecli/__init__.py:
 with open('euchrecli/__init__.py') as f:
     info = {}
     for line in f.readlines():
@@ -12,14 +12,21 @@ setup_info = dict(
     name='euchre-cli',
     version=info['version'],
     author='Bradley Wojcik',
+    author_email='bradleycwojcik@gmail.com',
     license='MIT',
     description='Play euchre in your terminal.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
+    url='https://github.com/bradleycwojcik/euchre-cli',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'click>=7'
+    ],
+    python_requires='>=3.7',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
     ],
     entry_points='''
         [console_scripts]
